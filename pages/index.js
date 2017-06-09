e5a266a8be2c86c342a849fecc9662805a499878
akupila <akupila@gmail.com>
2017-06-09 11:42:10 +0200

    add support for loading external stylesheets

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 // @flow
 
 import Layout from '../components/layout';
+import normalize from 'normalize.css/normalize.css';
 
 export default () =>
   <Layout>
     <h1>Systema</h1>
+    <style jsx global>{normalize}</style>
   </Layout>;
